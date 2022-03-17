a76d786d4fc8b284b18da53b17be222f462d1907
Kshitij Raj Sharma <skshitizraj@gmail.com>
2022-03-18 00:59:00 +0545

    added dynamic key value pair

--- assets/js/map.js
+++ assets/js/map.js
@@ -195,5 +195,21 @@ $(document).ready(function () {
     
       const form = document.querySelector("form");
       form.addEventListener("submit", handleSubmit);
+
+      //Clone the hidden element and shows it
+$('.add-one').click(function(){
+    $('.dynamic-element').first().clone().appendTo('.dynamic-stuff').show();
+    attach_delete();
+  });
+  
+  
+  //Attach functionality to delete buttons
+  function attach_delete(){
+    $('.delete').off();
+    $('.delete').click(function(){
+      console.log("click");
+      $(this).closest('.form-group').remove();
+    });
+}
       
 });
