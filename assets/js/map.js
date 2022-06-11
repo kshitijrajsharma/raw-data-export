f99ebf70cde98061415ffdd3ce99bcad6f6f0dc8
itskshitiz321 <skshitizraj@gmail.com>
2022-06-11 10:15:11 +0545

    loaded mb logic from byte to mb

--- assets/js/map.js
+++ assets/js/map.js
@@ -220,7 +220,7 @@ $(document).ready(function () {
           download_url[1].innerHTML =
             '<a id="response_file_download" href="' +
             data.download_url +
-            '"> Download </a><p><small>( Zip Size : '+data.zip_file_size+' MB, Inside File size : '+data.binded_file_size+' )</small></p>';
+            '"> Download </a><p><small>( Zip Size : '+data.zip_file_size/1000000+' MB, Inside File size : '+data.binded_file_size+' )</small></p>';
           document.getElementById("hot_export_btn").disabled = false;
           document.getElementById("loadgeojson").disabled = false;
           document.getElementById("geojsontextarea").disabled = false;
