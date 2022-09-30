838cd6d3c782b6467d28cfd72e824d1cdc715150
Kshitij Raj Sharma <skshitizraj@gmail.com>
2022-09-30 12:40:44 +0545

    added geopackage

--- assets/js/map.js
+++ assets/js/map.js
@@ -234,7 +234,7 @@ $(document).ready(function () {
       if (server=="prod"){
         api_url="https://galaxy-api.hotosm.org/v1/raw-data/current-snapshot/"
       }else {
-        api_url="http://127.0.0.1:8000/v1/raw-data/current-snapshot/"
+        api_url="http://127.0.0.1:4000/v1/raw-data/current-snapshot/"
       }
 
 
